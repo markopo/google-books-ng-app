5f715d6cb8af18ce6c9fd990b86619367910ead7
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-11-20 19:31:45 +0100

    Google books app

--- src/app/GoogleBook.ts
+++ src/app/GoogleBook.ts
@@ -3,10 +3,37 @@
 export class GoogleBook {
     public Id: number;
     public Title: string;
+    public Slug: string;
+    public Author: string;
+    public Publisher: string;
+    public PublishedDate: string;
+    public Description: string;
+    public Isbn10: string;
+    public Isbn13: string;
+    public PageCount: string;
+    public Thumbnail: string;
+    public InfoLink: string;
 
-    constructor(id: number, title: string) {
+    constructor(id: number, title: string, slug: string = '', author: string = '', publisher: string = '',
+                publishedDate: string = '', description: string = '', isbn13: string = '', isbn10: string = '',
+                pageCount: string = '', thumbNail: string = '', infoLink: string = '')
+    {
         this.Id = id;
         this.Title = title;
+        this.Slug = slug;
+        this.Author = author;
+        this.Publisher = publisher;
+        this.PublishedDate = publishedDate;
+        this.Description = description;
+        this.Isbn10 = isbn10;
+        this.Isbn13 = isbn13;
+        this.PageCount = pageCount;
+        this.Thumbnail = thumbNail;
+        this.InfoLink = infoLink;
+    }
+
+    public static Create() {
+        return new GoogleBook(0, '', '', '', '','','','','','','','');
     }
 
 
--- src/app/google-book-list/google-book-list.component.ts
+++ src/app/google-book-list/google-book-list.component.ts
@@ -9,11 +9,15 @@ import { GoogleBook } from '../GoogleBook';
 })
 export class GoogleBookListComponent implements OnInit {
 
-  books = [ new GoogleBook(1, 'title 1'), new GoogleBook(2, 'title 2'), new GoogleBook(3, 'title 3'),
-            new GoogleBook(4, 'title 4'), new GoogleBook(5, 'title 5'), new GoogleBook(6, 'title 6'),
-            new GoogleBook(7, 'title 7'), new GoogleBook(8, 'title 8'), new GoogleBook(9, 'title 9') ];
+  books = [];
 
-  constructor() { }
+  constructor() {
+
+      for(let i = 0; i <= 9; i++) {
+         this.books.push(GoogleBook.Create());
+      }
+
+  }
 
   ngOnInit() {
   }
